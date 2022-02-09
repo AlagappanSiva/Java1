ee32d7225912aa0ea5b687eb5a564b7552376101
AlagappanSiva <alagappan181099@gmail.com>
2022-02-09 21:11:46 +0530

    Added lines of code

---
+++ src/org/hub/Sample.java
@@ -0,0 +1,28 @@
+package org.hub;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("ALAGU");
+		
+		System.out.println("Sam");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
